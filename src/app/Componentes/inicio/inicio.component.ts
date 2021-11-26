3cf70aa71a34adf2163b52f6af07b5ef78994421
Teofilo <teofilo.garcia04@gmail.com>
2021-11-25 22:06:16 -0300

    Se agregaron cambios notables

--- src/app/Componentes/inicio/inicio.component.ts
+++ src/app/Componentes/inicio/inicio.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { animation, trigger, style, animate, transition, state } from '@angular/animations'
 import { translate } from '@angular/localize/src/utils';
+import Typed from 'typed.js';
+
+
 
 
 @Component({
@@ -25,15 +28,36 @@ import { translate } from '@angular/localize/src/utils';
   ]
 })
 export class InicioComponent implements OnInit {
+  
+  
 
   constructor() { 
+
+    
         
 
   }
 
   ngOnInit(): void {
 
+    const typed = new Typed('.typed', {
 
+      strings: [
+
+        '<i class="habilidades">Q.A. Analyst</i>',
+        '<i class="habilidades">Developer</i>'
+      
+      ],
+
+      //stringsElement: 
+      typeSpeed: 75,
+      startDelay: 1000,
+      backSpeed: 75,
+      shuffle: false,
+      backDelay: 1500,
+      loop: true,
+    
+    });
   }
 
 }
